775084064612e479b70cb221c71754bb4519a55f
Xiaoli Shen <alabebop@gmail.com>
2015-09-01 17:34:01 +0200

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'application#hello'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
