4f6f5d0dddf0d3e94048ce42b1d6b56efbf5499e
Xiaoli Shen <alabebop@gmail.com>
2015-09-12 14:59:20 +0200

    add email format test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,14 +4,21 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # create a valid user, 
+  # its passing should be a premise of 
+  # every other tests
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", 
+      email: "user@example.com")
   end
 
+  # test passing a valid user
   test "should be valid" do
     assert @user.valid?
   end
 
+  # test presence of name and email attr
   test "name should be present" do
     @user.name = " "
     assert_not @user.valid?
@@ -22,6 +29,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  # test max length 50 of name and 255 of email
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
@@ -32,5 +40,43 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  # test email format
+  test "email validation should accespt valid addreses" do
+    valid_addresses = %w[user@example.com USER@foo.COM
+      A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
 
+      # assert with a custom error message
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  # test invalid emails
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org
+      user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect}
+        should be invalid"
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
