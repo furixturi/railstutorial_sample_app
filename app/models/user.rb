d31dcd6ccd21ee89b7b275994ac507e36e54c1f4
Xiaoli Shen <alabebop@gmail.com>
2015-09-13 20:07:16 +0200

    ex-6-3 update email format regex to test out user@example..com is invalid

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
 
   # validate email presence, max length, format, and uniqueness
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255},
               format: { with: VALID_EMAIL_REGEX }, 
               uniqueness: { case_sensitive: false }
