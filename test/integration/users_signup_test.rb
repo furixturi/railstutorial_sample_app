08ab3a7f23e8e83e671d1c41f4aa84c0725f246b
Xiaoli Shen <alabebop@gmail.com>
2015-09-15 22:25:34 +0200

    add test valid user information submit

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,4 +17,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { # post_via_redirect follows the redirect after submission
+        name: "Example User",
+        email: "user@example.com",
+        password: "password",
+        password_confirmation: "password"
+      }
+    end
+    assert_template 'users/show'
+  end
 end
