142e490f4de368d171ed2cc3e624d355da656ae6
Xiaoli Shen <alabebop@gmail.com>
2015-09-15 19:56:42 +0200

    add sign up form: 4) add create action in user controller to handle post request to /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user]) # not final implementation
+    if @user.save
+      # handle successful save
+    else
+      render 'new'
+    end
+  end
+
 end
