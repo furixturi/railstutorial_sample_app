5bf1dbf31c2cc345114b9e0a6bc0b87f6cb5e84d
Xiaoli Shen <alabebop@gmail.com>
2015-09-16 09:53:11 +0200

    ex 7-2 test error messages

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,6 +16,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
   test "valid signup information" do
