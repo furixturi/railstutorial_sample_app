a4bfa53db88437d3533ec227a491e7509bd10b57
Xiaoli Shen <alabebop@gmail.com>
2015-09-16 21:34:22 +0200

    add login action create with user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #log in the user and redirect to user's show page
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
