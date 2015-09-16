6d695255e47370053a6ef2ea9dd82bbbfc888a8d
Xiaoli Shen <alabebop@gmail.com>
2015-09-16 22:03:33 +0200

    rewrite retrieving @current_user with || operator

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,10 +6,7 @@ module SessionsHelper
   end
 
   def current_user
-    if @current_user.nil?
-      @current_user = User.find_by(id: session[:user_id])
-    else
-      @current_user
+    @current_user = @current_user || User.find_by(id: session[:user_id])
     end
   end
 end
