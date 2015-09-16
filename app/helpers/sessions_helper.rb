0af0cdb7ac27a8e904b1e210ddfe16eac1fc722f
Xiaoli Shen <alabebop@gmail.com>
2015-09-16 22:08:22 +0200

    use idiomatic ||= operator instead of x = x || y

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,7 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user = @current_user || User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 end
