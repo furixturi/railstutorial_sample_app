ecb3d38a38da2da3ab0ed2d4119ac6e306d3c2e3
Xiaoli Shen <alabebop@gmail.com>
2015-09-16 22:59:52 +0200

    add User class method to return hash digest of a given string

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,10 @@ class User < ActiveRecord::Base
   #   password is correct, or "false" if otherwise
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
