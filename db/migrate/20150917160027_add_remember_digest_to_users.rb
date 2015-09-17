0ab4cca3d1e20570d39f6bfd33a3e01df40955f5
Xiaoli Shen <alabebop@gmail.com>
2015-09-17 18:02:24 +0200

    add generation command in migration file comment

--- db/migrate/20150917160027_add_remember_digest_to_users.rb
+++ db/migrate/20150917160027_add_remember_digest_to_users.rb
@@ -1,3 +1,5 @@
+# generation:
+# $ rails generate migration add_remember_digest_to_users remember_digest:string
 class AddRememberDigestToUsers < ActiveRecord::Migration
   def change
     add_column :users, :remember_digest, :string
