3268fa580b9289d5c7993550b574af6d87e62bb4
Xiaoli Shen <alabebop@gmail.com>
2015-09-17 17:21:03 +0200

    add assert messages in test_login_with_valid_information

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -12,12 +12,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with valid information" do
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
-    assert_redirected_to @user # assert redirected to user page
+    assert_redirected_to @user, "should redirect to user_path(@user)" # assert redirected to user page
     follow_redirect! # actually visit the redirected page
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_template 'users/show', "should render show user template"
+    assert_select "a[href=?]", login_path, { :count => 0 }, "log in link should be removed"
+    assert_select "a[href=?]", logout_path, {}, "log out link should be added"
+    assert_select "a[href=?]", user_path(@user), {}, "user profile link should be added"
+
+    # test log out
+
+
   end
 
   test "login with invalid information" do
