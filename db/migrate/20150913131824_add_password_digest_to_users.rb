5f4608e822087199e4dfb082eaae2715d1f9af85
Xiaoli Shen <alabebop@gmail.com>
2015-09-18 11:57:32 +0200

    write migration generation command to password_digest column

--- db/migrate/20150913131824_add_password_digest_to_users.rb
+++ db/migrate/20150913131824_add_password_digest_to_users.rb
@@ -1,3 +1,6 @@
+# generated with the command
+# $ rails generate migration add_password_digest_to_users password_digest:string
+
 class AddPasswordDigestToUsers < ActiveRecord::Migration
   def change
     add_column :users, :password_digest, :string
