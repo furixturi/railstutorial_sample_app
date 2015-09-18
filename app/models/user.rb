e1ddb181a9f2ea7a672502202885e0429086d36e
Xiaoli Shen <alabebop@gmail.com>
2015-09-18 14:39:34 +0200

    Remember login: add new_token in User model as class method to generate random token for cookie session

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,16 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  # ======== class methods
   # Returns the hash digest of the given string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
 end
