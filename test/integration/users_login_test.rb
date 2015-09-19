5015147ccae18b8436c88d0489820877efc1846a
furixturi <furixturi@gmail.com>
2015-09-19 21:05:12 +0200

    bug fix test double logout code

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -33,7 +33,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
-    assert_select "a[href=?]", user_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
 
   end
 
