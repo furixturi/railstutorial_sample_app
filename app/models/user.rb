582bcbdec3693ce47171cca48005de7d6fb80d97
furixturi <furixturi@gmail.com>
2015-09-20 20:01:36 +0200

    use the second way to declare class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,13 +43,15 @@ class User < ActiveRecord::Base
 
   # ======== class methods
   # Returns the hash digest of the given string
-  def User.digest(string)
+  # def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token.
-  def User.new_token
+  # def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
