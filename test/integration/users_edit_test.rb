dd750b9a376050adc9375d632748515f25ad2610
furixturi <furixturi@gmail.com>
2015-09-21 11:20:45 +0200

    generate users edit integration test

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,9 @@
+# generation command:
+# $ rails generate integration_test users_edit
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
