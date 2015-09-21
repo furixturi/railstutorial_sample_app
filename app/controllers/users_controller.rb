88ed3b0827bc5a240dd059b0f2f432abb3b5ed0f
furixturi <furixturi@gmail.com>
2015-09-21 11:56:21 +0200

    add before filter in UsersController to authorize only logged in user to access edit and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 # sign up
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     # debugger # can debug with byebug in rails console
@@ -45,5 +46,11 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
   
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Pleaser log in."
+        redirect_to login_url
+      end
+    end
 
 end
