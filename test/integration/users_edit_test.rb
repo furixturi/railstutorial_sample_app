5362241d71838494edf5905320147b48551ee769
furixturi <furixturi@gmail.com>
2015-09-21 11:23:52 +0200

    add testing invalid update patch request

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,4 +6,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+  	@user = users(:michael)
+  end
+
+  test "unsuccessful edit" do
+  	get edit_user_path(@user)
+  	assert_template 'users/edit'
+  	patch user_path(@user), user: {
+  		name: "",
+  		email: "foo@invalid",
+  		password: "foo",
+  		password_confirmation: "bar"
+  	}
+  	assert_template 'users/edit'
+  end
 end
