551e011d7842ff2d65732439b505603fb06b90f0
furixturi <furixturi@gmail.com>
2015-09-22 13:04:17 +0200

    generate integration test users_index

---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,8 @@
+# $ rails generate integration_test users_index
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
