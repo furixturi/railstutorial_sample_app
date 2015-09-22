27f5c4f95aed6b6ee9478f6a7dc0d9ea7bbe04af
furixturi <furixturi@gmail.com>
2015-09-22 15:42:35 +0200

    test un-logged in user making delete request to user destroy action should be redirected to log in page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -47,4 +47,11 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
 end
