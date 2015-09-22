5737bf6c964a3553f4b6f9210383022369a2b43a
furixturi <furixturi@gmail.com>
2015-09-22 12:42:46 +0200

    try per_page param of paginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], per_page: 20)
   end
 
   def show
