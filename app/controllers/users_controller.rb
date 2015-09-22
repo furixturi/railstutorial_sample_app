e9d23d8ddaf7e513b364295e34937200aadd583c
furixturi <furixturi@gmail.com>
2015-09-22 15:28:28 +0200

    add private method to confirm current_user is admin, otherwise redirect to root. Use it in before filter for action destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-
+  before_action :admin_user, only: :destroy
   def index
     @users = User.paginate(page: params[:page], per_page: 20)
   end
@@ -73,4 +73,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
    end
+
+   # confirm an admin user
+   def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+  
 end
