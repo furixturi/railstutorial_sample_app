fe98a90d70e50516719f69b58ef6b61be64f6e9c
furixturi <furixturi@gmail.com>
2015-09-22 15:12:21 +0200

    add default: false to admin field

--- db/migrate/20150922131022_add_admin_to_users.rb
+++ db/migrate/20150922131022_add_admin_to_users.rb
@@ -1,6 +1,6 @@
 # $ rails generate migration add_admin_to_users admin:boolean
 class AddAdminToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, default: false
   end
 end
