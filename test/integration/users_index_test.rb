3083e90d61f19b2a8279b7e580383288bee89460
furixturi <furixturi@gmail.com>
2015-09-22 15:57:33 +0200

    add test in integration test user index to test admin user should see users links and delete links for users other than himself, and should be able to delete a user

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -8,6 +8,8 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @admin = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test "index including pagination" do
@@ -19,6 +21,25 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
       assert_select 'a[href=?]', user_path(user), {
           text: user.name
         }, "cannot find #{user_path(user)}, #{user.name}"
+    end
+  end
+
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    
+    first_page_of_users = User.paginate(page:1, per_page: 20)    
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end      
+    end
+
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
     end
   end
 end
