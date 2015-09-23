4c4820280f59e0e430a13a9b0a2198b6db7ce968
furixturi <furixturi@gmail.com>
2015-09-23 19:41:10 +0200

    require user to have an activated account at log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create # submit login form post request
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
-      #log in the user and redirect to the url that the user tried to access before logging in, otherwise to user's show page
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user # redirect_back_or is defined in sessions_helper.rb
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+
+      if user.activated?
+        #log in the user and redirect to the url that the user tried to access before logging in, otherwise to user's show page
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user # redirect_back_or is defined in sessions_helper.rb
+      else
+        flash[:warning] = "Account not activated. Check your email for the activation link"
+        redirect_to root_url
+      end
     else
       # flash.now show message only in rendered page
       flash.now[:danger] = 'Invalid email/password combination'
