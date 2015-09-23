e9dae6c05742de60f158af592a9f7633dc6581b7
furixturi <furixturi@gmail.com>
2015-09-23 21:04:38 +0200

    add comments to note routes to the actions in PasswordResetsController

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,8 +1,14 @@
 # $ rails generate controller PasswordResets new edit --no-test-framework
 class PasswordResetsController < ApplicationController
-  def new
+  def new # get new_password_reset_path /password_resets/new
   end
 
-  def edit
+  def create # post password_resets_path /password_resets
+  end
+
+  def edit # get edit_password_reset_path(token) /password_resets/<token>/edit
+  end
+
+  def update # patch password_reset_path(token) /password_resets/<token>
   end
 end
