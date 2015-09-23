9a7046b765d085e9565e407cabea271a7005fcad
Xiaoli Shen <alabebop@gmail.com>
2015-09-23 16:11:29 +0200

    update mailer test so that the reset password test doesn't raise error

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -17,7 +17,7 @@ class UserMailerTest < ActionMailer::TestCase
     mail = UserMailer.password_reset
     assert_equal "Password reset", mail.subject
     assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["noreply@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
