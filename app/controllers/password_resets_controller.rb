e1a8d67bf079f802062bcd219bbd135f284f0d86
furixturi <furixturi@gmail.com>
2015-09-23 20:53:59 +0200

    generate RessetPasswords controller with new and edit actions

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,8 @@
+# $ rails generate controller PasswordResets new edit --no-test-framework
+class PasswordResetsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
   
   get 'help' => 'static_pages#help'
