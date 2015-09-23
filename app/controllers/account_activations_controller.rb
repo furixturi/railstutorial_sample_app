af139a05e180c950d2ebd946728011cbf1c59470
furixturi <furixturi@gmail.com>
2015-09-23 20:27:24 +0200

    bug fix AccountActivationsController

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -12,7 +12,7 @@ class AccountActivationsController < ApplicationController
       redirect_to user
     else
       flash[:danger] = "Invalid activation link"
-      redirect_to root
+      redirect_to root_url
     end
   
   end
