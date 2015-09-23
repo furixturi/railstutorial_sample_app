da7bbc292ecea13b90d5421a23a41e11ab0dc163
furixturi <furixturi@gmail.com>
2015-09-23 22:29:18 +0200

    implement create action for password reset form submit

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,6 +4,16 @@ class PasswordResetsController < ApplicationController
   end
 
   def create # post password_resets_path /password_resets
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Email address not found"
+      render 'new'
+    end
   end
 
   def edit # get edit_password_reset_path(token) /password_resets/<token>/edit
