14f1a596aa44ee947bdcaa73df28616f95d47238
Xiaoli Shen <alabebop@gmail.com>
2015-09-23 16:10:32 +0200

    write edit action in AccountActivationsController and use the rewritten generic user.authenticated? method

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,3 +1,8 @@
 # $ rails generate controller AccountActivations --no-test-framework
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && user.authenticated?(:activation, params[:id])
+    end
+  end
 end
