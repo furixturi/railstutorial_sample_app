f4f5c01e5c8170e5fe70537262f8098be4334cbb
furixturi <furixturi@gmail.com>
2015-09-26 22:28:27 +0200

    order microposts from newest to oldest

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,9 @@
 # $ rails generate model Micropost content:text user:references
 class Micropost < ActiveRecord::Base
   belongs_to :user
+  # order the microposts from newest to oldest
+  default_scope -> { order(created_at: :desc) }
+  
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
