32de4b78dc2bd1091cdfdce01741698e245413e6
furixturi <furixturi@gmail.com>
2015-09-26 22:07:53 +0200

    correct typo

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    @micropost = @user.nucrioists.build(content: "Lorem ipsum")
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do 
