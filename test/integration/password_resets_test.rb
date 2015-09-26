4cb262020963537e164a94281d89a83d852b0df9
furixturi <furixturi@gmail.com>
2015-09-26 13:23:22 +0200

    ex 10-1 integration test for password reset token expired

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -68,4 +68,25 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to user
   end
+
+  # token expired
+  test "expired token" do
+    get new_password_reset_path
+    post password_resets_path, password_reset: { email: @user.email }
+
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+
+    # patch password_reset_path(@user.reset_token), 
+    #  email: @user.email,
+    #  user: { password: "foobar",
+    #          password_confirmation: "foobar"
+    #        }
+    
+    get edit_password_reset_path(@user.reset_token), email: @user.email
+    assert_response :redirect, "should redirect to new_password_reset_url"
+    follow_redirect!
+    assert_match /expired/i, response.body, "response html should contain flash danger message \"Password reset has expired\""
+
+  end
 end
