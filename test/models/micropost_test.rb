4c9aacbcebe8684afa52cdda2a63bdab2d1a8ad0
furixturi <furixturi@gmail.com>
2015-09-26 22:01:40 +0200

    use @user.microposts.build to construct a micropost so that its user_id is automatically set

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.nucrioists.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do 
