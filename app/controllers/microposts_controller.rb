a1e1a283f54a4fd35379cf75a912016720fa66f3
furixturi <furixturi@gmail.com>
2015-09-28 12:14:03 +0200

    add private method micropost_params to micropost controller and use it to build micropost for current_user in create action

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,11 +1,23 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
-  
+
   # POST /microposts (microposts_path)
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   # DELETE /microposts/<id> (micropost_path(micropost))
   def destroy
   end
+
+  private
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
