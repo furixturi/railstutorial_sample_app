b1e6afed089b81767ebba0a7d77774679986ce5e
furixturi <furixturi@gmail.com>
2015-09-28 12:09:27 +0200

    add create and destroy action to microposts controller and use logged_in_user in before filter for both actions

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,11 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  
+  # POST /microposts (microposts_path)
+  def create
+  end
+
+  # DELETE /microposts/<id> (micropost_path(micropost))
+  def destroy
+  end
 end
