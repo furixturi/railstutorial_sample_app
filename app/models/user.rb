b38b93aa24392f1e58ccc8cb43e63ba60e69ec76
furixturi <furixturi@gmail.com>
2015-10-01 23:11:20 +0200

    add passive relationships and followers through passive_relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,22 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+
+  # user follows other users
+  # user is the foreign_key, 
+  # is in "follower" column
+  # the other users are in the "followed" column
   has_many :active_relationships, class_name: "Relationship", 
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :following, through: :active_relationships,
+                       source: :followed
+
+  # user has followers
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships,
+                       source: :follower
                                   
   attr_accessor :remember_token, :activation_token, :reset_token
   # neutralize user submitted email cases
@@ -80,7 +94,23 @@ class User < ActiveRecord::Base
     Micropost.where("user_id=?", id)
   end
 
-  # private methods
+  # Follows a user
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  # ========= private methods
+
   private
     def downcase_email
       email.downcase!
