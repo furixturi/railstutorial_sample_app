a4e7efbb00e81685bf766f167332e1f669050636
furixturi <furixturi@gmail.com>
2015-10-02 23:02:40 +0200

    correct typo

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   resources :microposts, only: [:create, :destroy]
 
   # relationships
-  resources :relationships, only [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
