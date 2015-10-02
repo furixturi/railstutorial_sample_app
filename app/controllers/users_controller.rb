3d85e1357cf4f868597a218f7e54d819918c0ac8
furixturi <furixturi@gmail.com>
2015-10-02 23:48:52 +0200

    require user login for following and followers actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 # sign up, update, delete user
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   def index
