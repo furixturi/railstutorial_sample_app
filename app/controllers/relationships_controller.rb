c08155698274d388f77cac6082c9d0ee82592310
furixturi <furixturi@gmail.com>
2015-10-03 13:57:38 +0200

    add before_action logged_in_user filter to Relationships controller, add create and destroy action definition

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,3 +1,10 @@
 # $ rails generate controller Relationships
 class RelationshipsController < ApplicationController
+  before_action :logged_in_user
+
+  def create
+  end
+
+  def destroy
+  end
 end
