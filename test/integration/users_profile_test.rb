91525414705bc9b0340d3986ab7843fd61c6324b
furixturi <furixturi@gmail.com>
2015-10-03 23:25:33 +0200

    ex-12-1 test user stats

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -22,4 +22,12 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
       assert_match micropost.content, response.body
     end
   end
+
+  test "user stats" do
+    get user_path(@user)
+    assert_select '#following', text: @user.following.count.to_s
+    assert_select '#followers', text: @user.followers.count.to_s
+    assert_select 'a[href=?]', following_user_path(@user)
+    assert_select 'a[href=?]', followers_user_path(@user)
+  end
 end
